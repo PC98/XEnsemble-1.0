526be3506b2770aadf6aca9e9478890989550fb0
Prabhav Chawla <prabhav_chawla@yahoo.co.in>
2020-11-18 11:57:42 -0600

    minor changes to script

--- run_one_attack.sh
+++ run_one_attack.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
-dataset="CIFAR-10"
+dataset="MNIST"
 model="CNN1"
-attack="fgsm?eps=0.3;"
+attack="fgsm?eps=0.3"
+label_index=0
 
-python main_attack_portal.py --dataset_name $dataset --model_name $model --nb_examples=1 --attacks "$attack"
+python main_attack_portal_2.py --dataset_name $dataset --label_index=$label_index --model_name $model --nb_examples=1 --attacks $attack
